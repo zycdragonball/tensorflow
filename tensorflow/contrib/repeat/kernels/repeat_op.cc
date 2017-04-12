c710a64282d85222cc795065d403523bab97793c
zycdragonball <zycdragonball@gmail.com>
2017-04-12 10:55:21 -0400

    Add files via upload

--- tensorflow/contrib/repeat/kernels/repeat_op.cc
+++ tensorflow/contrib/repeat/kernels/repeat_op.cc
@@ -19,74 +19,16 @@ limitations under the License.
 #include "tensorflow/core/framework/tensor_types.h"
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/kernels/bounds_check.h"
+#include "tensorflow/contrib/repeat/kernels/repeat_op.h"
 
 namespace tensorflow{
 
-int64 fetch_input_offset(int64 output_offset,
-                         int64 group_input_size,
-                         int64 group_output_size,
-                         int64 batch_size,
-                         Eigen::Tensor<int32, 1> batch_index) {
-  int64 group_offset = output_offset / group_output_size;
-  int64 batch_offset = batch_index((output_offset%group_output_size) / batch_size);
-  int64 elem_offset = output_offset % batch_size;
-  
-  return group_offset*group_input_size + batch_offset*batch_size + elem_offset;
-}
+typedef Eigen::ThreadPoolDevice CPUDevice;
+#if GOOGLE_CUDA
+typedef Eigen::GpuDevice GPUDevice;
+#endif // GOOGLE_CUDA
 
-Eigen::Tensor<int32, 1> fetch_batch_index(
-    typename TTypes<int32>::ConstFlat repeats_flat,
-    int size) {
-  Eigen::Tensor<int32, 1> result({size});
-  int ptr = 0;
-  const int N = repeats_flat.size();
-  for (int i = 0; i < N; ++i) {
-    int repeat = repeats_flat(i);
-    for (int j = 0; j < repeat; ++j) {
-      result(ptr) = i;
-      ptr++;
-    }
-  }
-  
-  return result;
-}
-
-template <typename T>
-void RepeatCPUImpl(const Tensor& input,
-                   typename TTypes<int32>::ConstFlat repeats_flat,
-                   int axis, Tensor* output, int old_dim, int new_dim) {
-  auto input_flat = input.flat<T>();
-  auto output_flat = output->flat<T>();
-  
-  //A batch is inner dimensions < axis
-  //A group is inner dimensions <= axis
-  int64 batch_size = 1;
-  int32 dims = input.shape().dims();
-  for (int32 i = axis + 1; i < dims; ++i) {
-    batch_size *= input.shape().dim_size(i);
-  }
-  
-  int64 out_num_elem = output->NumElements();
-  if (repeats_flat.size() == 1) {
-    for (int64 i = 0; i < out_num_elem; ++i) {
-      int64 batch_offset = i / (batch_size*repeats_flat(0));
-      int64 elem_offset = i % batch_size;
-      output_flat(i) = input_flat(batch_offset*batch_size + elem_offset);
-    }
-  } else {
-    auto batch_index = fetch_batch_index(repeats_flat, new_dim);
-    int64 group_input_size = batch_size * old_dim;
-    int64 group_output_size = batch_size * new_dim;
-    for (int64 i = 0; i < out_num_elem; ++i) {
-      output_flat(i) = input_flat(fetch_input_offset(i, group_input_size,
-                                                     group_output_size,
-                                                     batch_size,
-                                                     batch_index));
-    }
-  }
-}
-
-template <typename T>
+template <typename Device, typename T>
 class RepeatOp : public OpKernel {
  public:
   explicit RepeatOp(OpKernelConstruction* context) : OpKernel(context) {
@@ -126,7 +68,16 @@ class RepeatOp : public OpKernel {
     Tensor* output = NULL;
     OP_REQUIRES_OK(context, context->allocate_output(0, output_shape, &output));
     
-    RepeatCPUImpl<T>(input, repeats_flat, axis_, output, old_dim, new_dim);    
+
+#if GOOGLE_CUDA
+    if (std::is_same<Device, GPUDevice>::value) {
+      RepeatGPUImpl<T>(context->eigen_gpu_device(), input, repeats_flat, axis_, output);
+      return ;
+    }
+#endif // GOOGLE_CUDA
+
+    RepeatCPUImplV2<T>(context->device(), input, repeats_flat,
+                       axis_, 10000, output); 
   }
   
  private:
@@ -142,9 +93,26 @@ class RepeatOp : public OpKernel {
       Name("Repeat")                             \
       .Device(DEVICE_CPU)                        \
       .TypeConstraint<type>("T"),                \
-      RepeatOp<type>)
+      RepeatOp<CPUDevice, type>)
 
 TF_CALL_ALL_TYPES(REGISTER_KERNEL);
 
 #undef REGISTER_KERNEL
+
+#if GOOGLE_CUDA
+
+#define REGISTER_KERNEL_GPU(type)                \
+  REGISTER_KERNEL_BUILDER(                       \
+      Name("Repeat")                             \
+      .Device(DEVICE_GPU)                        \
+      .TypeConstraint<type>("T")                 \
+      .HostMemory("repeats"),                    \
+      RepeatOp<GPUDevice, type>)
+
+TF_CALL_GPU_NUMBER_TYPES(REGISTER_KERNEL_GPU);
+
+#undef REGISTER_KERNEL_GPU
+
+#endif // GOOGLE_CUDA
+
 } //end namespace tensorflow
---
+++ tensorflow/contrib/repeat/kernels/repeat_op_cpu_impl.cc
@@ -0,0 +1,232 @@
+/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/register_types.h"
+#include "tensorflow/core/framework/tensor_types.h"
+#include "tensorflow/core/util/work_sharder.h"
+
+namespace tensorflow{
+
+namespace {
+
+template <typename T>
+struct MemCopier {
+  
+  inline void Copy(const T* input, T* output, int64 size){
+    std::copy(input, input + size, output);
+  }
+};
+
+} //end namespace
+
+template <typename T>
+void RepeatCPUImpl(const Tensor& input,
+                   const typename TTypes<int32>::ConstFlat& repeats_flat,
+                   int axis, Tensor* output) {
+  auto input_flat = input.flat<T>();
+  auto output_flat = output->flat<T>();
+  MemCopier<T> copier;
+    
+  //A batch is inner dimensions > axis
+  size_t batch_size = 1;
+  int32 dims = input.shape().dims();
+  for (int32 i = axis + 1; i < dims; ++i) {
+    batch_size *= input.shape().dim_size(i);
+  }
+  int64 num_batch = input_flat.size() / batch_size;
+  
+  const T* in = input_flat.data();
+  T* out = output_flat.data();
+  if (repeats_flat.size() == 1) {
+    for (int64 i = 0; i < num_batch; ++i) {
+      int32 repeat = repeats_flat(0);
+      for (int64 j = 0; j < repeat; ++j) {
+        copier.Copy(in, out, batch_size);
+        out += batch_size;
+      }
+      in += batch_size;
+    }
+  } else {
+    for (int64 i = 0; i < num_batch; ++i) {
+      int32 repeat = repeats_flat(i % repeats_flat.size());
+      for (int64 j = 0; j < repeat; ++j) {
+        copier.Copy(in, out, batch_size);
+        out += batch_size;
+      }
+      in += batch_size;
+    }
+  }
+}
+
+template <typename T>
+void RepeatCPUImplV2(DeviceBase* d, const Tensor& input,
+                     const typename TTypes<int32>::ConstFlat& repeats_flat,
+                     int axis, int64 cost_per_unit, Tensor* output) {
+  auto input_flat = input.flat<T>();
+  auto output_flat = output->flat<T>();
+  MemCopier<T> copier;
+  
+  //A batch is inner dimensions > axis
+  //A group is inner dimensions >= axis
+  int64 batch_size = 1;
+  int32 dims = input.shape().dims();
+  for (int32 i = axis + 1; i < dims; ++i) {
+    batch_size *= input.shape().dim_size(i);
+  }
+  int64 group_pre_size = batch_size * input.shape().dim_size(axis);
+  int64 group_size = batch_size * output->shape().dim_size(axis);
+  
+  auto worker_threads = d->tensorflow_cpu_worker_threads();
+  int num_threads = std::min(4, worker_threads->num_threads);
+  // strings define a different amount of work (generally much more) compared
+  // with standard POD, so we parallelize differently.
+  if (!std::is_same<T, string>::value) {
+    num_threads =
+        static_cast<int>(std::min<int64>(num_threads, output_flat.size() / 4096));
+  }
+  
+  auto work = [input_flat, repeats_flat, axis, &copier,
+               batch_size, group_pre_size, group_size, &output_flat](
+      int64 start, int64 end) {
+    const T* in = input_flat.data();
+    T* out = output_flat.data();
+    T* out_start = out + start;
+    T* out_end = out + end;
+              
+    if (repeats_flat.size() == 1) {
+      int64 out_batch_size = batch_size * repeats_flat(0);
+      in += (start/out_batch_size) * batch_size;
+      out += (start/batch_size) * batch_size;
+      
+      //handle partial out_batch at start
+      if (start % out_batch_size != 0) {
+        int64 offset = start % batch_size;
+        //handle partial batch at start
+        if (offset != 0) {
+          if (out + batch_size > out_end) {
+            copier.Copy(in + offset, out_start, (out_end-out) - offset);
+            return ;
+          }
+          copier.Copy(in + offset, out_start, batch_size - offset);
+          out += batch_size;
+        }
+        
+        int64 repeat_skip = (out-output_flat.data())/batch_size % repeats_flat(0);
+        for(int64 i = repeat_skip; i < repeats_flat(0); ++i) {
+          if (out + batch_size > out_end) {
+            copier.Copy(in, out, out_end - out);
+            return ;
+          }
+          copier.Copy(in, out, batch_size);
+          out += batch_size;
+        }
+        in += batch_size;
+      }
+      
+      //handle remaining data
+      int64 batch_to_cpy = (out_end-out) / out_batch_size + 1;
+      for (int64 i = 0; i < batch_to_cpy; ++i) {
+        for (int64 j = 0; j < repeats_flat(0); ++j) {
+          if (out + batch_size > out_end) {
+            copier.Copy(in, out, out_end - out);
+            return ;
+          }
+          copier.Copy(in, out, batch_size);
+          out += batch_size;
+        }
+        in += batch_size;
+      }
+      
+    } else {
+      int64 skip_group = start / group_size;
+      in += skip_group * group_pre_size;
+      out += skip_group * group_size;      
+      
+      //handle partial group at start
+      bool started = false;
+      if (start % group_size != 0) {
+        for (int64 j = 0; j < repeats_flat.size(); ++j) {
+          for (int64 k = 0; k < repeats_flat(j); ++k) {
+            if (out + batch_size <= out_start) {
+              out += batch_size;
+              continue;
+            }
+            
+            if (started) {
+              if (out + batch_size > out_end) {
+                copier.Copy(in, out, out_end - out);
+                return ;
+              }
+              copier.Copy(in, out, batch_size);
+            } else {
+              int64 offset = out_start-out;
+              if (out + batch_size > out_end) {
+                copier.Copy(in + offset, out_start, (out_end-out) - offset);
+                return ;
+              }
+              copier.Copy(in + offset, out_start, batch_size - offset);
+              started = true;
+            }
+            
+            out += batch_size;
+          }
+          in += batch_size;
+        }
+      }
+      
+      //handle remaining data
+      int64 group_to_cpy = (out_end-out) / group_size + 1;
+      for (int64 i = 0; i < group_to_cpy; ++i) {
+        for (int64 j = 0; j < repeats_flat.size(); ++j) {
+          for (int64 k = 0;  k < repeats_flat(j); ++k) {
+            if (out + batch_size > out_end) {
+              copier.Copy(in, out, out_end - out);
+              return ;
+            }
+            copier.Copy(in, out, batch_size);
+            out += batch_size;
+          }
+          in += batch_size;
+        }
+      }
+    }
+  };
+
+  Shard(worker_threads->num_threads, worker_threads->workers, output_flat.size(),
+        cost_per_unit, work);
+}
+
+#define REGISTER(T)                                          \
+template void RepeatCPUImpl<T>(                              \
+    const Tensor& input,                                     \
+    const typename TTypes<int32>::ConstFlat& repeats_flat,   \
+    int axis, Tensor* output);
+
+#define REGISTER_V2(T)                                       \
+template void RepeatCPUImplV2<T>(                            \
+    DeviceBase* d, const Tensor& input,                      \
+    const typename TTypes<int32>::ConstFlat& repeats_flat,   \
+    int axis, int64 cost_per_unit, Tensor* output);
+    
+TF_CALL_ALL_TYPES(REGISTER);
+TF_CALL_ALL_TYPES(REGISTER_V2);
+
+#undef REGISTER
+#undef REGISTER_V2
+
+} //end namespace tensorflow
